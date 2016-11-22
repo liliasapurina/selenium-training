43a1e261653520d90630d1cb9b43e9428c753e48
Lilia.Sapurina <Lilia.Sapurina@billing.ru>
2016-11-23 00:35:56 +0300

    Добавлен тест на прокликивание меню админки

---
+++ tests/walking_in_admin.js
@@ -0,0 +1,71 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    test = require('selenium-webdriver/testing'),
+    assert = require('selenium-webdriver/testing/assert'),
+    common = require('../utils/common');
+
+test.describe('Проход по всем разделам админки', function () {
+  var driver,
+      user = {
+        name : 'admin',
+        password : 'admin'
+      };
+
+  test.before(function () {
+    driver = new webdriver.Builder()
+        .forBrowser('chrome')
+        .build();
+  });
+
+  test.it('Открытие главной страницы', function () {
+    driver.get('http://localhost/litecart/admin/');
+  });
+
+  test.it('Вход в учетную запись', function () {
+    driver.findElement(By.name('username')).sendKeys(user.name);
+    driver.findElement(By.name('password')).sendKeys(user.password);
+    driver.findElement(By.name('login')).click();
+  });
+
+  test.it('Прокликивание всех пунктов меню', function () {
+
+    driver.findElement(By.id('box-apps-menu')).findElements(By.tagName('li'))
+        .then(function (elements) {
+          var menuLength = elements.length;
+
+          common.forStartToEnd(1, menuLength, function (i) {
+            // Выюор пункта мею
+            var menu = driver.findElement(By.id('box-apps-menu')).findElement(By.xpath('./li[' + i + ']'));
+            menu.getText().then(function(text){
+              console.info('Нажат пункт: ' + text);
+            });
+            menu.click();
+            driver.findElement(By.id('box-apps-menu')).findElements(By.xpath('./li[' + i + ']//li'))
+                .then(function (elements) {
+                  var subMenuLength = elements.length;
+
+                  common.forStartToEnd(1, subMenuLength, function (j) {
+                    // Выбор подпункта меню
+                    var subMenu = driver.findElement(By.id('box-apps-menu')).findElement(By.xpath('./li[' + i + ']//li[' + j + ']'));
+                    subMenu.getText().then(function(text){
+                      console.info('Нажат подпункт: ' + text);
+                    });
+                    subMenu.click();
+                  });
+
+                  driver.findElements(By.css('[id=content] h1')).then(function(head){
+                      if(head.length === 0){
+                        console.error('У страницы нет заголовка!');
+                      }
+                  });
+                });
+          });
+        });
+
+  });
+
+  test.after(function () {
+    driver.quit();
+  });
+
+});
--- utils/common.js
+++ utils/common.js
@@ -4,14 +4,27 @@
 
 module.exports = {
 
+  /*
+   Генерация уникального email
+   */
   generateEmail : function () {
     var text = '',
         charSet = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-    for( var i = 0; i < 10; i++ )
+    for (var i = 0; i < 10; i++)
       text += charSet.charAt(Math.floor(Math.random() * charSet.length));
 
     return text + '@po4ta.ru';
+  },
+
+  /*
+   Имитация цикла for для асинхронных действий
+   */
+  forStartToEnd : function (start, end, todo) {
+    if (start <= end) {
+      todo(start);
+      this.forStartToEnd(start + 1, end, todo);
+    }
   }
 
 };
